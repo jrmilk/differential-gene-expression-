ad7399a296bfae441d0e08297fa8abd0fcafc1ea
jrmilk <90703121+jrmilk@users.noreply.github.com>
2026-05-11 14:47:00 -0300

    Create summarize_UP_DOWN.py

---
+++ summarize_UP_DOWN.py
@@ -0,0 +1,16 @@
+             DE_CB49...   DE_SP78...
+Ss_UP            80          120
+Ss_DOWN          67          104
+
+So_UP           300          710
+So_DOWN         274          680
+
+Both_UP         280          620
+Both_DOWN       249          618
+
+NA_UP             0            0
+NA_DOWN           0            0
+
+Total_UP        660         1450
+Total_DOWN      590         1402
+Total_DE       1250         2852
